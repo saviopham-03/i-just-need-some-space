ba6c33525405fdc5102b08874820d92e83b061a3
nu0x08537D3dx <nayeer.u.alam@gmail.com>
2026-08-08 22:00:54 +1000

    health bar functioning
    Health bar working with fire damage (stacks) and expandable for other damage types and timings.

--- space-game/Scripts/Menus/ingame_ui.gd
+++ space-game/Scripts/Menus/ingame_ui.gd
@@ -1,9 +1,12 @@
 extends Control
 
+@onready var ship_health_bar = $ShipHealthBar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	ship_health_bar.add_ship_bar(0, "FIRE")
+	await get_tree().create_timer(1).timeout
+	ship_health_bar.add_ship_bar(1, "FIRE")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
